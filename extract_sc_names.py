703ac3e5cea09ec364b9cb93815468f13e776613
Mahmudur <mahmudhera93@gmail.com>
2024-01-16 16:28:39 -0500

    also storing the genome names

--- extract_sc_names.py
+++ extract_sc_names.py
@@ -6,6 +6,8 @@ genomes_directory_names = '/scratch/mbr5797/genomes_extracted_from_kegg'
 if __name__ == '__main__':
     directory_names = [name for name in os.listdir(genomes_directory_names) if os.path.isdir(os.path.join(genomes_directory_names, name))]
     random.shuffle(directory_names)
+    f2 = open('scientific_name_to_genome_name.csv', 'w')
+    f2.write('scientific_name,genome_name\n')
     with open('scientific_names.txt', 'w') as f:
         for directory_name in directory_names:
             genome_file_name = os.path.join(genomes_directory_names, directory_name, directory_name+'.fasta')
@@ -15,3 +17,5 @@ if __name__ == '__main__':
             sc_name = ' '.join(first_line.split(' ')[2:4])
             print(sc_name + ' extracted from ' + directory_name)
             f.write(sc_name + '\n')
+            f2.write(sc_name + ',' + directory_name + '\n')
+    f2.close()
