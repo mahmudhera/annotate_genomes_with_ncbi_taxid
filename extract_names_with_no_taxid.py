940b387116e9be97c63c5d50173f604e4ecd5aee
Mahmudur <mahmudhera93@gmail.com>
2024-01-16 16:33:06 -0500

    use tab sep

--- extract_names_with_no_taxid.py
+++ extract_names_with_no_taxid.py
@@ -1,8 +1,15 @@
 import pandas as pd
 
 if __name__ == '__main__':
+    df = pd.read_csv('scientific_name_to_genome_name.csv', sep=',')
+    scientific_name_list = df['scientific_name'].tolist()
+    genome_name_list = df['genome_name'].tolist()
+    sc_name_to_ge_name = {}
+    for scientific_name, genome_name in list( zip(scientific_name_list, genome_name_list) ):
+        sc_name_to_ge_name[scientific_name] = genome_name
+
     df = pd.read_csv('list_of_taxids', sep='\t', header=None)
     df.columns = ['scientific_name', 'taxid']
     for scientific_name, taxid in list( zip(df['scientific_name'].tolist(), df['taxid'].tolist()) ):
         if str(taxid) == 'nan':
-            print(scientific_name)
+            print(scientific_name, sc_name_to_ge_name[scientific_name])
--- extract_sc_names.py
+++ extract_sc_names.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     directory_names = [name for name in os.listdir(genomes_directory_names) if os.path.isdir(os.path.join(genomes_directory_names, name))]
     random.shuffle(directory_names)
     f2 = open('scientific_name_to_genome_name.csv', 'w')
-    f2.write('scientific_name,genome_name\n')
+    f2.write('scientific_name\tgenome_name\n')
     with open('scientific_names.txt', 'w') as f:
         for directory_name in directory_names:
             genome_file_name = os.path.join(genomes_directory_names, directory_name, directory_name+'.fasta')
@@ -17,5 +17,5 @@ if __name__ == '__main__':
             sc_name = ' '.join(first_line.split(' ')[2:4])
             print(sc_name + ' extracted from ' + directory_name)
             f.write(sc_name + '\n')
-            f2.write(sc_name + ',' + directory_name + '\n')
+            f2.write(sc_name + '\t' + directory_name + '\n')
     f2.close()
