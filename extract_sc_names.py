6145fb4e4d4226e83a76ccd33a93a09de3fd33c8
Mahmudur <mahmudhera93@gmail.com>
2024-01-16 15:38:26 -0500

    added code to read files

--- extract_sc_names.py
+++ extract_sc_names.py
@@ -3,5 +3,12 @@ import os
 genomes_directory_names = '/scratch/mbr5797/genomes_extracted_from_kegg'
 
 if __name__ == '__main__':
-    directories = [name for name in os.listdir(genomes_directory_names) if os.path.isdir(os.path.join(genomes_directory_names, name))]
-    print(directories[:10])
+    directory_names = [name for name in os.listdir(genomes_directory_names) if os.path.isdir(os.path.join(genomes_directory_names, name))]
+    with open('sc_names.txt', 'w') as f:
+        for directory_name in directory_names[:10]:
+            genome_file_name = os.path.join(genomes_directory_names, directory_name, directory_name+'.fasta')
+            with open(genome_file_name, 'r') as g:
+                first_line = g.readline()
+                first_line = first_line.strip()
+                print(first_line)
+            f.write(first_line+'\n')
